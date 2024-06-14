993e193238fc9342a804d8b397247100420ba352
Akshay <akshay@bpract.com>
2024-06-14 20:33:02 +0530

    added start and end time stamp to the task contract

--- interfaces/ITodoList.sol
+++ interfaces/ITodoList.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 
 interface ITodoList {
 	function getCurrentTokenHolding() external view returns(uint256);
-	function addTask(string memory) external;
+	function addTask(string memory, uint256) external;
 	/*function removeTask(uint) external;*/
 	function markComplete(uint) external;
 	function getTaskIds() external view returns(uint[] memory);
