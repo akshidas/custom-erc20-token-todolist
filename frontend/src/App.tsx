91f7bcf1f1851c60c0e8593fd9e0732c09f686fb
Akshay <akshay@bpract.com>
2024-06-20 21:15:38 +0530

    added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, Typography } from "@mui/material";
+
+const App = () => {
+    return (
+        <>
+            <Typography variant="h1">Todo List</Typography>
+            <Button>Connect</Button>
+        </>
+    );
+};
+
+export default App;
